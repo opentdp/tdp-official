d904009a012f9c28e68c53ef688042c58a430995
若海 <wang@rehiy.com>
2023-07-20 17:38:50 +0800

    fix: 博文无法打开

--- modules/route.php
+++ modules/route.php
@@ -4,7 +4,7 @@ $router = new Altorouter();
 
 // 路由形参
 
-$rtkey = ['method' => '', 'route' => '', 'target' => '', 'name' => ''];
+$rtkeys = ['method' => '', 'route' => '', 'target' => '', 'name' => ''];
 
 // 默认首页
 
@@ -28,8 +28,8 @@ $index = App::storage('index');
 
 foreach ((array)$index as $item) {
     foreach ($item->routes as $route) {
-        $router->map($route['method'], $route['route'], function ($args) use ($route, $rtkey) {
-            $extra = array_diff_key($route, $rtkey);
+        $router->map($route['method'], $route['route'], function ($args) use ($route, $rtkeys) {
+            $extra = array_diff_key($route, $rtkeys);
             $model = new $route['target']();
             $model->view($args + $extra);
             return $model;
