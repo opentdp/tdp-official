bdb67e8566fb4b8d5f8ca9358ae23dd1f46f1ac2
若海 <wang@rehiy.com>
2023-07-21 18:58:08 +0800

    feat: 增加产品列表

---
+++ modules/products/model.php
@@ -0,0 +1,6 @@
+<?php
+
+class ProductsModel extends BasicModel
+{
+    protected $name = 'products';
+}
---
+++ modules/products/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="zh-Hans-CN">
+
+<head>
+    <?php $this->need('basic/header.php'); ?>
+    <?php $this->need('assets/css/blog.css'); ?>
+</head>
+
+<body>
+    <?php $this->need('basic/loader.php'); ?>
+    <?php $this->need('basic/navbar.php'); ?>
+    <main id="main">
+        <?php $this->need('widget/breadcrumb.php'); ?>
+        <?php $this->need('product/products.php'); ?>
+    </main>
+    <?php $this->need('basic/footer.php'); ?>
+</body>
+
+</html>
