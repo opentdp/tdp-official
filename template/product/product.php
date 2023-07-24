fdaf40aec0e5903c25816293f02c52615eeb2cd7
若海 <wang@rehiy.com>
2023-07-24 19:21:38 +0800

    perf: 更改图片列表字段

--- template/product/product.php
+++ template/product/product.php
@@ -6,19 +6,11 @@
             <div class="col-lg-8">
                 <div class="product-details-slider swiper">
                     <div class="swiper-wrapper align-items-center">
-
-                        <div class="swiper-slide">
-                            <img src="assets/img/product/1/details-1.jpg" alt="">
-                        </div>
-
-                        <div class="swiper-slide">
-                            <img src="assets/img/product/1/details-2.jpg" alt="">
-                        </div>
-
-                        <div class="swiper-slide">
-                            <img src="assets/img/product/1/details-3.jpg" alt="">
-                        </div>
-
+                        <?php foreach ($this->product['images'] as $image) { ?>
+                            <div class="swiper-slide">
+                                <img src="<?= $image; ?>" alt="">
+                            </div>
+                        <?php } ?>
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
@@ -28,19 +20,14 @@
                 <div class="product-info">
                     <h3>产品信息</h3>
                     <ul>
-                        <li><strong>名称</strong>: TDP Cloud</li>
-                        <li><strong>版本</strong>: Version 0.13.0</li>
-                        <li><strong>发布日期</strong>: 2023-06-01</li>
-                        <li><strong>演示地址</strong>: <a href="https://cloud.opentdp.org">cloud.opentdp.org</a></li>
+                        <li><strong>名称</strong>：<?= $this->product['name']; ?></li>
+                        <li><strong>版本</strong>：Version <?= $this->product['version']; ?></li>
+                        <li><strong>发布日期</strong>：<?= $this->product['time']; ?></li>
+                        <li><strong>演示地址</strong>：<a href="<?= $this->product['url']; ?>" target="_blank"><?= $this->product['url']; ?></a></li>
                     </ul>
                 </div>
                 <div class="product-description">
-                    <h2>可以跨平台部署的云资源管理面板</h2>
-                    <p>支持管理多个云账号资源</p>
-                    <p>支持添加子节点 (TDP Worker)</p>
-                    <p>支持自动签发SSL证书</p>
-                    <p>支持 WebSSH 终端及密钥对管理</p>
-                    <p>支持敏感数据加密存储（3DES）</p>
+                    <?= $this->product['content']; ?>
                 </div>
             </div>
 
--- template/product/products.php
+++ template/product/products.php
@@ -18,169 +18,25 @@
         </div>
 
         <div class="row product-container" data-aos="fade-up" data-aos-delay="400">
-
-            <div class="col-lg-4 col-md-6 product-item filter-app">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-1.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-1.jpg" data-gallery="productGallery" class="product-lightbox" title="App 1">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
+            <?php foreach ($this->products as $item) { ?>
+                <div class="col-lg-4 col-md-6 product-item filter-app">
+                    <div class="product-wrap">
+                        <img src="<?= $item['thumb']; ?>" class="img-fluid" alt="">
+                        <div class="product-info">
+                            <h4><?= $item['name']; ?></h4>
+                            <p><?= $item['summary']; ?></p>
+                            <div class="product-links">
+                                <a href="<?= $item['thumb']; ?>" data-gallery="productGallery" class="product-lightbox" title="<?= $item['tags']; ?>">
+                                    <i class="bx bx-plus"></i>
+                                </a>
+                                <a href="index.php?rt=/product/<?= $item['id']; ?>" title="产品详情">
+                                    <i class="bx bx-link"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-web">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-2.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Web 3</h4>
-                        <p>Web</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-2.jpg" data-gallery="productGallery" class="product-lightbox" title="Web 3">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-app">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-3.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>App 2</h4>
-                        <p>App</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-3.jpg" data-gallery="productGallery" class="product-lightbox" title="App 2">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-card">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-4.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Card 2</h4>
-                        <p>Card</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-4.jpg" data-gallery="productGallery" class="product-lightbox" title="Card 2">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-web">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-5.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Web 2</h4>
-                        <p>Web</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-5.jpg" data-gallery="productGallery" class="product-lightbox" title="Web 2">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-app">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-6.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>App 3</h4>
-                        <p>App</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-6.jpg" data-gallery="productGallery" class="product-lightbox" title="App 3">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-card">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-7.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Card 1</h4>
-                        <p>Card</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-7.jpg" data-gallery="productGallery" class="product-lightbox" title="Card 1">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-card">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-8.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Card 3</h4>
-                        <p>Card</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-8.jpg" data-gallery="productGallery" class="product-lightbox" title="Card 3">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 product-item filter-web">
-                <div class="product-wrap">
-                    <img src="assets/img/product/thumb-9.jpg" class="img-fluid" alt="">
-                    <div class="product-info">
-                        <h4>Web 3</h4>
-                        <p>Web</p>
-                        <div class="product-links">
-                            <a href="assets/img/product/thumb-9.jpg" data-gallery="productGallery" class="product-lightbox" title="Web 3">
-                                <i class="bx bx-plus"></i>
-                            </a>
-                            <a href="index.php?rt=/product/1" title="More Details">
-                                <i class="bx bx-link"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <?php } ?>
         </div>
 
     </div>
