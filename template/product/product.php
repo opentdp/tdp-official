55af13e19500f2b66d4de85de76b312f90280ec8
若海 <wang@rehiy.com>
2023-07-25 08:59:19 +0800

    style: 优化样式

--- template/product/product.php
+++ template/product/product.php
@@ -3,6 +3,18 @@
 
         <div class="row gy-4">
 
+            <div class="col-lg-4 order-lg-last">
+                <div class="product-info">
+                    <h3>产品信息</h3>
+                    <ul>
+                        <li><strong>名称</strong>：<?= $this->product['name']; ?></li>
+                        <li><strong>版本</strong>：Version <?= $this->product['version']; ?></li>
+                        <li><strong>发布日期</strong>：<?= $this->product['time']; ?></li>
+                        <li><strong>演示地址</strong>：<a href="<?= $this->product['url']; ?>" target="_blank"><?= $this->product['url']; ?></a></li>
+                    </ul>
+                </div>
+            </div>
+
             <div class="col-lg-8">
                 <div class="product-details-slider swiper">
                     <div class="swiper-wrapper align-items-center">
@@ -13,18 +25,6 @@
                         <?php } ?>
                     </div>
                     <div class="swiper-pagination"></div>
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="product-info">
-                    <h3>产品信息</h3>
-                    <ul>
-                        <li><strong>名称</strong>：<?= $this->product['name']; ?></li>
-                        <li><strong>版本</strong>：Version <?= $this->product['version']; ?></li>
-                        <li><strong>发布日期</strong>：<?= $this->product['time']; ?></li>
-                        <li><strong>演示地址</strong>：<a href="<?= $this->product['url']; ?>" target="_blank"><?= $this->product['url']; ?></a></li>
-                    </ul>
                 </div>
                 <div class="product-description">
                     <?= $this->product['content']; ?>
