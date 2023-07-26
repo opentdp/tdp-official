98c361679c440e6b2d2c74a54e1bd3d38346f312
若海 <wang@rehiy.com>
2023-07-26 08:43:07 +0800

    feat: 团队数据化

--- modules/home/model.php
+++ modules/home/model.php
@@ -10,12 +10,17 @@ class HomeModel extends BasicModel
     protected $service = [];
     protected $service_meta = [];
 
+    protected $team = [];
+    protected $team_meta = [];
+
     public function view($args)
     {
         $this->news = App::cache('news/index');
         $this->news_meta = App::cache('news/meta');
         $this->service = App::cache('service/index');
         $this->service_meta = App::cache('service/meta');
+        $this->team = App::cache('team/index');
+        $this->team_meta = App::cache('team/meta');
         // 设置模板变量
         $this->title = '官方网站';
     }
--- template/home/team.php
+++ template/home/team.php
@@ -2,132 +2,38 @@
     <div class="container">
 
         <div class="section-title" data-aos="fade-up">
-            <h2>团队成员</h2>
-            <p>充满活力和创造力的团队，拥有丰富的经验和技能，致力于为客户提供优质的服务</p>
+            <h2><?= $this->team_meta['title']; ?></h2>
+            <p><?= $this->team_meta['description']; ?></p>
         </div>
 
         <div class="row">
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/rehiy" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            <?php foreach ($this->team as $item) { ?>
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="<?= $item['thumb']; ?>" class="img-fluid" alt="">
+                            <div class="social">
+                                <a class="github" href="<?= $item['github']; ?>" target="_blank">
+                                    <i class="bi bi-github"></i>
+                                </a>
+                                <a class="twitter" href="<?= $item['twitter']; ?>" target="_blank">
+                                    <i class="bi bi-twitter"></i>
+                                </a>
+                                <a class="facebook" href="<?= $item['facebook']; ?>" target="_blank">
+                                    <i class="bi bi-facebook"></i>
+                                </a>
+                                <a class="linkedin" href="<?= $item['linkedin']; ?>" target="_blank">
+                                    <i class="bi bi-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4><?= $item['author']; ?></h4>
+                            <span><?= $item['title']; ?></span>
                         </div>
                     </div>
-                    <div class="member-info">
-                        <h4>若海</h4>
-                        <span>全栈工程师</span>
-                    </div>
                 </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="200">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/hhucqian" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>熊</h4>
-                        <span>全栈工程师</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="300">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/jcomey" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>田</h4>
-                        <span>资深产品</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="300">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-5.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/chinatings" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>King</h4>
-                        <span>资深产品</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="400">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/KincaidYang" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>小泽</h4>
-                        <span>社群运营</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="400">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href="https://github.com/54ayao" class="github" target="_blank">
-                                <i class="bi bi-github"></i>
-                            </a>
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>鹰瑶</h4>
-                        <span>社群运营</span>
-                    </div>
-                </div>
-            </div>
-
+            <?php } ?>
         </div>
 
     </div>
