c97f37eb131e5084b46b58207a9a5c2e5842af82
若海 <wang@rehiy.com>
2023-08-01 17:03:00 +0800

    feat: 重建缓存后输出提醒

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('APP_ROOT', strtr(__DIR__, '\\', '/') . '/');
 require APP_ROOT . 'library/autoload.php';
 
 if (App::cache('meta', time() - 86400) === null) {
-    App::obtain('RuntimeModel')->build();
+    App::obtain('AppRuntime')->build();
 }
 
 App::boot($argv[1] ?? reset($_GET));
--- modules/runtime/model.php
+++ modules/runtime/model.php
@@ -4,13 +4,9 @@ class RuntimeModel extends BasicModel
 {
     protected $name = 'runtime';
 
-    /**
-     * 重建缓存
-     * @return void
-     */
     public function build()
     {
         App::obtain('AppRuntime')->build();
-        $this->site = App::cache('meta');
+        App::obtain('ErrorModel')->message('ok');
     }
 }
